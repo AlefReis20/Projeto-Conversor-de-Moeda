d5582fe8a6c7e579debf51a0a733f99d788f61d0
AlefReis20 <aleftec@outlook.com>
2022-09-27 00:25:35 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+const dolar = 5.39
+const euro = 5.19
+const bitcoin = 0.0000093
+
+const convertValues = () => {
+    const inputReais = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const currencyValueText = document.getElementById('currency-value-text')
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(inputReais)
+
+    if (select.value === 'US$ Dolar Americano') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(inputReais / dolar)
+    } 
+
+    if (select.value === '€ Euro') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+        }).format(inputReais / euro)
+    }
+
+    if (select.value === 'Bitcoin') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'BTC'
+        }).format(inputReais * bitcoin)
+    }
+}
+
+changeCurrency = () => {
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === 'US$ Dolar Americano') {
+        currencyName.innerHTML = 'Dolar Americano'
+        currencyImg.src = './assets/dolar.png'
+    }
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = 'Euro'
+        currencyImg.src = './assets/euro.png'
+    }
+
+    if (select.value === 'Bitcoin') {
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = './assets/bitcoin.png'
+    }
+    convertValues()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
